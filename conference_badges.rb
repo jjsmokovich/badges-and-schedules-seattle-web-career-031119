d849f4f54a5998e1daae18923185233d803fde97
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-28 08:54:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,14 +3,7 @@ def badge_maker(name)
 end
 
 def badge_batch_creator(names)
-  event = []
-  
-  names.each do |name|
-    event.push("Hello, my name is #{name}.")
-  end
-  return event
-end
-
+ 
 def assign_rooms(speakers)
   
 end
