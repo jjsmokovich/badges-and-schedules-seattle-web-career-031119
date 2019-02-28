05b80268d30c703f9574c9718c8e80775f57a020
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-28 09:00:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,10 +15,7 @@ end
 def assign_rooms(speakers)
   assignment =[]
   
-  speakers.each_with_index{|speaker,index|
-  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
-    }
-   return assignment
+  
 end
 
 def printer
